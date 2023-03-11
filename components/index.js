042d7b32b387496bbcf00e553e99f592db09bd26
chazwilder <cwilder@chazwilder.onmicrosoft.com>
2023-03-11 15:32:32 -0500

    Completed Hot Bids Part 1!

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
 import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
 
-export { NavBar, Footer, Button, Banner, CreatorCard };
+export { NavBar, Footer, Button, Banner, CreatorCard, NFTCard };
 
