0967aa27816b9e0d7411e38864f3ffc1f5d8de69
chazwilder <cwilder@chazwilder.onmicrosoft.com>
2023-03-12 19:01:50 -0400

    Saving - Errand Run and Forgot Where I Left Off LOL

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -1,9 +1,24 @@
+
 import React, { useState, useEffect } from 'react';
 import Web3Modal from 'web3modal';
 import { ethers } from 'ethers';
 import axios from 'axios';
 
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+
 import { MarketAddress, MarketAddressAbi } from './constants';
+
+const projectId = '2Mva4CdjwzbxCk5CRIeWB42jUM4';
+const projectSecret = '1ad0f83065d49d9eb99069ea12304ec0';
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString('base64')}`;
+
+const client = ipfsHttpClient({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  headers: {
+    authorization: auth,
+  } });
 
 export const NFTContext = React.createContext();
 
@@ -14,7 +29,6 @@ export const NFTProvider = ({ children }) => {
   const checkIfWalletIsConnected = async () => {
     if (!window.ethereum) return alert('Get MetaMask!');
     const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-    console.log('currentAccount', { accounts });
 
     if (accounts.length !== 0) {
       setcurrentAccount(accounts[0]);
@@ -34,8 +48,18 @@ export const NFTProvider = ({ children }) => {
     window.location.reload();
   };
 
+  const uploadToIPFS = async (file) => {
+    try {
+      const added = await client.add({ content: file });
+      const url = `https://cw-nft-marketplace.infura-ipfs.io/ipfs/${added.path}`;
+      return url;
+    } catch (error) {
+      console.log('Error uploading file to IPFS.', error);
+    }
+  };
+
   return (
-    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
+    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}>
       {children}
     </NFTContext.Provider>
   );
